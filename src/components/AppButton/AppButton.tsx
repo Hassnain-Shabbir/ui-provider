34e910e1a7fcda59fc589e5cfe19b221f3fbd9ef
Hassnain-Shabbir <hassnain.shabbir@hashlogics.com>
2024-04-26 17:07:10 +0000

    update the button props

--- src/components/AppButton/AppButton.tsx
+++ src/components/AppButton/AppButton.tsx
@@ -3,11 +3,31 @@ import { Button } from "@mui/material"
 
 interface AppButtonProps {
   children: React.ReactNode
-  [key: string]: any
+  variant: "text" | "contained" | "outlined"
+  href: string
+  disabled: boolean
+  color:
+    | "inherit"
+    | "primary"
+    | "secondary"
+    | "success"
+    | "error"
+    | "info"
+    | "warning"
 }
 
-const AppButton = ({ children, ...props }: AppButtonProps) => {
-  return <Button {...props}>{children}</Button>
+const AppButton = ({
+  children,
+  variant,
+  href,
+  disabled,
+  color,
+}: AppButtonProps) => {
+  return (
+    <Button variant={variant} color={color} href={href} disabled={disabled}>
+      {children}
+    </Button>
+  )
 }
 
 export default AppButton
